c8071b74fb4aebc262858cc8c79d5653b80ee1ff
Nadagani Amulya Prasanth <amulyaprasanth301@gmail.com>
2022-10-16 21:51:20 +0530

    completed robofriends project

--- react/robofriends/src/index.js
+++ react/robofriends/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './containers/App';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+ReactDOM.createRoot(container).render(<App />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
