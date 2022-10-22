dff9daab701a59ff4141866d4477e50f90e42adf
Nadagani Amulya Prasanth <amulyaprasanth301@gmail.com>
2022-10-22 16:57:17 +0530

    started smartbrain project

---
+++ javascript/advanced-js/es8_async_await.js
@@ -0,0 +1,20 @@
+// ASYNC AWAIT
+
+const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums'
+]
+
+const getData = async function() {
+    try {
+        const [ users, posts, albums ] = await Promise.all(urls.map(url =>
+            fetch(url).then(resp => resp.json())
+        ))
+            console.log('users', users)
+            console.log('posts', posts)
+            console.log('albums', albums)
+    } catch {
+        console.log('oops')
+    }
+    }
